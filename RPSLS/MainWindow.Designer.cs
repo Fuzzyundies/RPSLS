dfa966c39d51e92e4d04124dd42144c626e820f1
Fuzzyundies <Eric.Jonathon.Lambert@gmail.com>
2016-01-19 20:41:25 -0800

    Initial Commit
    Building it

---
+++ RPSLS/MainWindow.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace RPSLS
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.Rock = new System.Windows.Forms.Button();
+            this.Paper = new System.Windows.Forms.Button();
+            this.Scissors = new System.Windows.Forms.Button();
+            this.Lizard = new System.Windows.Forms.Button();
+            this.Spock = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Rock
+            // 
+            resources.ApplyResources(this.Rock, "Rock");
+            this.Rock.Name = "Rock";
+            this.Rock.Tag = "1";
+            this.Rock.UseVisualStyleBackColor = true;
+            this.Rock.Click += new System.EventHandler(this.GameButtonClick);
+            // 
+            // Paper
+            // 
+            resources.ApplyResources(this.Paper, "Paper");
+            this.Paper.Name = "Paper";
+            this.Paper.Tag = "2";
+            this.Paper.UseVisualStyleBackColor = true;
+            this.Paper.Click += new System.EventHandler(this.GameButtonClick);
+            // 
+            // Scissors
+            // 
+            resources.ApplyResources(this.Scissors, "Scissors");
+            this.Scissors.Name = "Scissors";
+            this.Scissors.Tag = "3";
+            this.Scissors.UseVisualStyleBackColor = true;
+            this.Scissors.Click += new System.EventHandler(this.GameButtonClick);
+            // 
+            // Lizard
+            // 
+            resources.ApplyResources(this.Lizard, "Lizard");
+            this.Lizard.Name = "Lizard";
+            this.Lizard.Tag = "4";
+            this.Lizard.UseVisualStyleBackColor = true;
+            this.Lizard.Click += new System.EventHandler(this.GameButtonClick);
+            // 
+            // Spock
+            // 
+            resources.ApplyResources(this.Spock, "Spock");
+            this.Spock.Name = "Spock";
+            this.Spock.Tag = "5";
+            this.Spock.UseVisualStyleBackColor = true;
+            this.Spock.Click += new System.EventHandler(this.GameButtonClick);
+            // 
+            // MainWindow
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Spock);
+            this.Controls.Add(this.Lizard);
+            this.Controls.Add(this.Scissors);
+            this.Controls.Add(this.Paper);
+            this.Controls.Add(this.Rock);
+            this.Name = "MainWindow";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Rock;
+        private System.Windows.Forms.Button Paper;
+        private System.Windows.Forms.Button Scissors;
+        private System.Windows.Forms.Button Lizard;
+        private System.Windows.Forms.Button Spock;
+    }
+}
+
---
+++ RPSLS/MainWindow.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPSLS
+{
+
+    public enum Shoot
+    {
+        Rock = 1, //Automatically incremetes by 1.
+        Paper,
+        Scissors,
+        Lizard,
+        Spock
+    };
+
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GameButtonClick(object sender, EventArgs e)
+        {
+            // int v = (int)(sender as Button).Tag;
+
+            int v = Convert.ToInt32((sender as Button).Tag); //Takes the tag, converting to a number.
+
+            Shoot playerShoot = (Shoot)v;
+
+            Random rng = new Random();
+
+            int randomNumber = rng.Next(1, 6);
+
+            Shoot computerShoot = (Shoot)randomNumber;
+
+            // MessageBox.Show(playerShoot.ToString());
+
+            switch(playerShoot)
+            {
+                case Shoot.Paper:
+
+                    if (computerShoot == Shoot.Rock)
+                    {
+                        PlayerWins();
+                    }
+
+                    else if (computerShoot == Shoot.Lizard)
+                    {
+                        PlayerLoses();
+                    }
+
+                    else if (computerShoot == Shoot.Spock)
+                    {
+                        PlayerWins();
+                    }
+
+                    else if (computerShoot == Shoot.Scissors)
+                    {
+                        PlayerLoses();
+                    }
+
+                    else if (computerShoot == Shoot.Paper)
+                    {
+                        PlayerDraws();
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void PlayerWins()
+        {
+            MessageBox.Show("You Win!");
+        }
+
+        private void PlayerLoses()
+        {
+            MessageBox.Show("You Lose!");
+        }
+
+        private void PlayerDraws()
+        {
+            MessageBox.Show("You Draw!");
+        }
+    }
+}
